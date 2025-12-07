2d030c73ef742c8ad401be5610ef02b86b1b7520
NaremanDayya <nanadaya166@gmail.com>
2025-12-07 14:24:19 +0200

    chat list clients selection

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -52,7 +52,7 @@ class Conversation extends Model
 
         return $this->unreadMessagesCount();
     }
-//test
+
     public function getReceiver()
     {
         if ($this->sender_id === Auth::id()) {

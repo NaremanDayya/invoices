8f6933bb70709323564b091aa42ddd0fbee43991
NaremanDayya <nanadaya166@gmail.com>
2025-12-21 11:14:20 +0200

    exporting logic

---
+++ resources/views/components/export-dropdown.blade.php
@@ -0,0 +1,20 @@
+<div class="dropdown d-inline-block ms-2">
+    <button class="btn btn-outline-success dropdown-toggle" type="button" id="exportDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+        <i class="fas fa-download me-2"></i>
+        تصدير البيانات
+    </button>
+    <ul class="dropdown-menu" aria-labelledby="exportDropdown">
+        <li>
+            <a class="dropdown-item export-pdf" href="#" data-type="pdf">
+                <i class="fas fa-file-pdf text-danger me-2"></i>
+                تصدير PDF
+            </a>
+        </li>
+        <li>
+            <a class="dropdown-item export-excel" href="#" data-type="excel">
+                <i class="fas fa-file-excel text-success me-2"></i>
+                تصدير Excel
+            </a>
+        </li>
+    </ul>
+</div>
